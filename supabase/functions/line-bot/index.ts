500af6425ef52d8d63524f7aca5cda2f58e0af84
4geru <westhouse51@gmail.com>
2024-03-17 23:03:34 +0900

    save photo in supabase storage

--- supabase/functions/line-bot/index.ts
+++ supabase/functions/line-bot/index.ts
@@ -3,24 +3,97 @@
 // This enables autocomplete, go to definition, etc.
 
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
+import { supabaseClient } from './supabaseClient.ts'
 import { replyMessage } from './messages.ts'
 console.log("Hello from Functions!")
 
 serve(async (req) => {
   const { events } = await req.json()
   console.log(events)
-  if (events && events[0]?.type === "message") {
-    // 文字列化したメッセージデータ
+
+  if (events && events[0]?.type === "message" && events[0]?.message.type === "image") {
+    const filePath = `${events[0].source.userId}/photo/${events[0].message.id}.jpg` // 画像の保存先のpathを指定
+    // リクエストヘッダー
+    const headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + Deno.env.get('LINE_CHANNEL_ACCESS_TOKEN')
+    }
+
+    // https://developers.line.biz/ja/docs/messaging-api/nodejs-sample/#send-reply
+    await fetch(`https://api-data.line.me/v2/bot/message/${events[0].message.id}/content`,
+        {
+            method: "GET",
+            headers: headers,
+        }
+    ).then(async (response) => {
+      const blob = await response.blob();
+      console.log(response)
+      // https://supabase.com/docs/reference/javascript/storage-from-upload
+      const { data, error } = await supabaseClient(req)
+        .storage
+        .from('photo')
+        .upload(filePath, blob, {
+          contentType: 'image/jpg'
+        })
+      console.log(data, error)
+
+      replyMessage(events, [{type: "text", text: "画像を保存しました"}])
+    });
+  }
+  if (events && events[0]?.type === "message" && events[0]?.message.type === "text") {
+    // https://supabase.com/docs/reference/javascript/storage-from-list
+    const { data, error } = await supabaseClient(req)
+      .storage
+      .from('photo')
+      .list('public', {
+        limit: 10,
+        offset: 0,
+        sortBy: { column: 'created_at', order: 'desc' }
+      })
+    console.log({data, error})
+
+    const images = data.filter(item =>
+      item.name.match(/jpg/g) ||
+      item.name.match(/jpeg/g) ||
+      item.name.match(/png/g)
+    )
+    const buttonTemplate = {
+        "type": "template",
+        "altText": "This is a buttons template",
+        "template": {
+          "type": "buttons",
+          "text": "Please select image",
+          "actions": images.slice(0, 4).map(item => ({
+            "type": "postback",
+            "label": item.name.slice(0, 10),
+            "data": JSON.stringify({name: item.name})
+          }))
+        }
+      }
+
+    let messages:any = [buttonTemplate]
+    replyMessage(events, messages)
+  }
+
+  if (events && events[0]?.type === "postback") {
+    const postbackData = JSON.parse(events[0].postback.data)
+    // https://supabase.com/docs/reference/javascript/storage-from-getpublicurl
+    const { data } = supabaseClient(req)
+     .storage
+     .from('photo')
+     .getPublicUrl(`${events[0].source.userId}/photo${postbackData.name}`)
     let messages:any = [
       {
         "type": "text",
-        "text": "こんにちは！"
+        "text": JSON.stringify(data)
       },
       {
-        "type": "text",
-        "text": "テスト / test で単語を登録できます"
+        "type": "image",
+        "originalContentUrl": data.publicUrl,
+        "previewImageUrl": data.publicUrl
       }
     ]
+
     replyMessage(events, messages)
   }
 
